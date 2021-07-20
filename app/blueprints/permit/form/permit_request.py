65f3950aa4c6c1cd6349d35672495d913327142d
Javed Wright <jdwright870@gmail.com>
2021-07-20 10:51:33 -0500

    restrict AQI range

--- app/blueprints/permit/form/permit_request.py
+++ app/blueprints/permit/form/permit_request.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField, DecimalField
-from wtforms.validators import InputRequired, DataRequired, Regexp
+from wtforms.validators import InputRequired, DataRequired, Regexp, NumberRange
 from app.constants import CONSTANTS
 
 
@@ -9,7 +9,8 @@ class PermitRequestForm(FlaskForm):
     town = SelectField("Town", choices=[], validators=[InputRequired()])
     area_name = StringField("Area Name", validators=[InputRequired(), Regexp(r'^[^!@#$%^&*()+\-=\[\]{};\':"\\|,.<>\/?]*$')])
     square_footage = DecimalField("Square Footage", validators=[InputRequired(), DataRequired(message=CONSTANTS["NUMBER_REQUIRED_VALIDATOR_MESSAGE"])])
-    AQI = DecimalField("AQI", validators=[InputRequired(), DataRequired(message=CONSTANTS["NUMBER_REQUIRED_VALIDATOR_MESSAGE"])])
+    AQI = DecimalField("AQI", validators=[InputRequired(), DataRequired(message=CONSTANTS["NUMBER_REQUIRED_VALIDATOR_MESSAGE"]),
+                                          NumberRange(min=0, max=500, message="Please enter a number between 0 and 500")])
     PM25 = DecimalField("PM 2.5", validators=[InputRequired(), DataRequired(message=CONSTANTS["NUMBER_REQUIRED_VALIDATOR_MESSAGE"])])
     PM10 = DecimalField("PM 10", validators=[InputRequired(), DataRequired(message=CONSTANTS["NUMBER_REQUIRED_VALIDATOR_MESSAGE"])])
     CO = DecimalField("CO", validators=[InputRequired(), DataRequired(message=CONSTANTS["NUMBER_REQUIRED_VALIDATOR_MESSAGE"])])
