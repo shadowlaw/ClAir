882e75e7363cdb81d6aa57b2f7e982538a4ec945
Saran Wilson <sarandionnewilson@gmail.com>
2021-07-20 22:02:22 -0600

    Add Login UI

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,6 @@
 from flask.blueprints import Blueprint
+from flask import render_template, request
+
 
 main = Blueprint("main", __name__)
 
@@ -6,3 +8,17 @@ main = Blueprint("main", __name__)
 @main.route("/")
 def home():
     return "home page"
+
+@main.route("/login", methods = ['GET', 'POST'])
+def login():
+    return render_template("login.html")
+    if request.method == 'POST' and form.validate():
+        if form.username == 'billybob' and form.password == 'help':
+            flash ('You have been logged in!', 'success')
+            return redirect(url_for('main.home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+            return render_template('login.html', title = 'Login', form = form)
+
+
+
