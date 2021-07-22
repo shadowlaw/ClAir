77ea146daea120fe2d8cba26452b45f8970b2b37
Saran Wilson <sarandionnewilson@gmail.com>
2021-07-21 22:42:28 -0600

    Add Login Functionality

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ db = SQLAlchemy()
 csrf = CSRFProtect()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
-login_manager.login_view = 'login'
+login_manager.login_view = 'main.login'
 
 
 def create_app(config=Config()):
@@ -18,6 +18,7 @@ def create_app(config=Config()):
 
     app.config.from_object(config)
     db.init_app(app)
+    login_manager.init_app(app)
     csrf.init_app(app)
     bcrypt.init_app(app)
 
--- app/blueprints/main/views.py
+++ app/blueprints/main/views.py
@@ -1,24 +1,43 @@
 from flask.blueprints import Blueprint
-from flask import render_template, request
-
+from flask import render_template, request, flash, redirect, url_for
+from app.blueprints.main.form.login import Login
+from app.blueprints.main.model.user import User
+from app import login_manager
+from flask_login import login_user, logout_user, login_required, current_user
+from werkzeug.security import check_password_hash
 
 main = Blueprint("main", __name__)
 
 
 @main.route("/")
+@login_required
 def home():
-    return "home page"
+    return render_template("dashboard.html")
 
 @main.route("/login", methods = ['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    form = Login()
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
     if request.method == 'POST' and form.validate():
-        if form.username == 'billybob' and form.password == 'help':
+        user = User.query.filter_by(username = form.username.data).first()
+        if user is not None and check_password_hash(user.password, form.password.data):
+            login_user(user)
             flash ('You have been logged in!', 'success')
             return redirect(url_for('main.home'))
         else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
-            return render_template('login.html', title = 'Login', form = form)
+            flash ('Login Unsuccessful. Check username and password', 'danger')
 
+    return render_template("login.html", form = form)
 
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+@main.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Logged out', 'success')
+    return redirect(url_for("main.login"))
 
