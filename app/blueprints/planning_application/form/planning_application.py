c33e1303e3ea7207ce6c7328cebbbd36e10a427c
Javed Wright <jdwright870@gmail.com>
2021-07-29 11:54:11 -0500

    fix pollutant names

--- app/blueprints/planning_application/form/planning_application.py
+++ app/blueprints/planning_application/form/planning_application.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField, DecimalField
 from wtforms.validators import InputRequired, DataRequired, Regexp, NumberRange
 from app.constants import CONSTANTS
+from flask import Markup
 
 
 class PlanningApplicationForm(FlaskForm):
@@ -11,10 +12,10 @@ class PlanningApplicationForm(FlaskForm):
     square_footage = DecimalField("Square Footage", validators=[InputRequired(), DataRequired(message=CONSTANTS["NUMBER_REQUIRED_VALIDATOR_MESSAGE"])])
     AQI = DecimalField("AQI", validators=[InputRequired(), DataRequired(message=CONSTANTS["NUMBER_REQUIRED_VALIDATOR_MESSAGE"]),
                                           NumberRange(min=0, max=500, message="Please enter a number between 0 and 500")])
-    PM25 = DecimalField("PM 2.5", validators=[InputRequired(), DataRequired(message=CONSTANTS["NUMBER_REQUIRED_VALIDATOR_MESSAGE"])])
-    PM10 = DecimalField("PM 10", validators=[InputRequired(), DataRequired(message=CONSTANTS["NUMBER_REQUIRED_VALIDATOR_MESSAGE"])])
-    CO = DecimalField("CO", validators=[InputRequired(), DataRequired(message=CONSTANTS["NUMBER_REQUIRED_VALIDATOR_MESSAGE"])])
-    NO2 = DecimalField("NO2", validators=[InputRequired(), DataRequired(message=CONSTANTS["NUMBER_REQUIRED_VALIDATOR_MESSAGE"])])
-    SO2 = DecimalField("SO2", validators=[InputRequired(), DataRequired(message=CONSTANTS["NUMBER_REQUIRED_VALIDATOR_MESSAGE"])])
-    O3 = DecimalField("O3", validators=[InputRequired(), DataRequired(message=CONSTANTS["NUMBER_REQUIRED_VALIDATOR_MESSAGE"])])
+    PM25 = DecimalField(Markup("PM<sub>2.5</sub>"), id='PM25', validators=[InputRequired(), DataRequired(message=CONSTANTS["NUMBER_REQUIRED_VALIDATOR_MESSAGE"])])
+    PM10 = DecimalField(Markup("PM<sub>10</sub>"), id='PM10', validators=[InputRequired(), DataRequired(message=CONSTANTS["NUMBER_REQUIRED_VALIDATOR_MESSAGE"])])
+    CO = DecimalField("CO", id='CO', validators=[InputRequired(), DataRequired(message=CONSTANTS["NUMBER_REQUIRED_VALIDATOR_MESSAGE"])])
+    NO2 = DecimalField(Markup("NO<sub>2</sub>"), id='NO2', validators=[InputRequired(), DataRequired(message=CONSTANTS["NUMBER_REQUIRED_VALIDATOR_MESSAGE"])])
+    SO2 = DecimalField(Markup("SO<sub>2</sub>"), id='SO2', validators=[InputRequired(), DataRequired(message=CONSTANTS["NUMBER_REQUIRED_VALIDATOR_MESSAGE"])])
+    O3 = DecimalField(Markup("O<sub>3</sub>"), id='O3', validators=[InputRequired(), DataRequired(message=CONSTANTS["NUMBER_REQUIRED_VALIDATOR_MESSAGE"])])
     submit = SubmitField("Submit Request")
