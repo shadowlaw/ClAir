9ed7f4d36e0909e06bddb7a8b9dd0286a501274a
Javed Wright <jdwright870@gmail.com>
2021-08-04 22:44:21 -0500

    update air quality upload ui

--- app/blueprints/admin/form/pollutant_upload_form.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileRequired, FileAllowed
-from wtforms import SubmitField
-
-
-class PollutantUploadForm(FlaskForm):
-    submit = SubmitField('Upload')
-    photo = FileField("Photo", validators=[FileRequired(), FileAllowed('csv',
-                                                                       "CSV Only!")])
--- app/blueprints/admin/views.py
+++ app/blueprints/admin/views.py
@@ -1,17 +1,18 @@
 from flask import Blueprint, render_template, request
 from flask_login import login_required
 
-from app.blueprints.admin.form.pollutant_upload_form import PollutantUploadForm
+from app.blueprints.admin.form.air_quality_upload_form import AirQualityUploadForm
 
 admin_views = Blueprint('admin_views', __name__)
 
 
-@admin_views.route('/pollutant/upload', methods=['GET', 'POST'])
+@admin_views.route('/air_quality/upload', methods=['GET', 'POST'])
+# add role restriction
 @login_required
-def upload_pollutant():
-    pollutant_upload = PollutantUploadForm()
+def air_quality_upload():
+    aq_upload = AirQualityUploadForm()
 
-    if request.method == 'POST' and pollutant_upload.validate():
+    if request.method == 'POST' and aq_upload.validate():
         pass
 
-    return render_template('pollutant_upload.html', upload_form=pollutant_upload)
+    return render_template('air_quality_upload.html', upload_form=aq_upload)
