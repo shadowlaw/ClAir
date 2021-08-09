01c642b360cbd5c273802759667e59b81c6f6617
Saran Wilson <sarandionnewilson@gmail.com>
2021-08-08 22:35:05 -0600

    Report Secondary Pollutants

--- app/blueprints/main/service/report_service.py
+++ app/blueprints/main/service/report_service.py
@@ -5,6 +5,7 @@ from app.blueprints.main.model.tree_efficacy import TreeEfficacy
 from app.blueprints.main.model.town_pollutant import TownPollutant
 
 from app.blueprints.report.model.report import Report
+from app.blueprints.report.model.targeted_pollutants import TargetedPollutant, TargetedPollutantEnum
 from app.blueprints.report.model.report_detail import ReportDetail
 
 from app.blueprints.planning_application.model.planning_application import PlanningApplication
@@ -24,7 +25,7 @@ def get_recommendations(square_footage,pollutants):
     results = {} # key: tree_id; value: [amount to plant, pollutant targeted]
     square_footage_left = square_footage
     # Known pollutants in order of severity
-    priority = {"Ozone" : 6, "PM2.5" : 5, "PM10": 4, "NO2": 3, "CO": 2, "SO2": 1, "AQI": 0}
+    priority = {"O3" : 6, "PM25" : 5, "PM10": 4, "NO2": 3, "CO": 2, "SO2": 1, "AQI": 0}
     # Sort pollutants in order of severity
     pollutants = sorted(pollutants, key=lambda x: priority[x.pollutant_id], reverse=True)
     # Remove any pollutant below safety limit
@@ -72,6 +73,7 @@ def get_recommendations(square_footage,pollutants):
             all_tree_pollutants = TreeEfficacy.query.filter_by(tree_id=best_tree.id).all()
 
             # Reduce the pollutant levels in the dict
+            ol_poll = list()
             for x in all_tree_pollutants:
                 if x.pollutant_id in p.keys():
                     p[x.pollutant_id] -= x.effectiveness
@@ -80,14 +82,15 @@ def get_recommendations(square_footage,pollutants):
                 results[best_tree.id][0] += 1
             else:
                 results[best_tree.id] = [1, f.id]
-    return results
+            ol_poll.append(f.id)
+    return results, ol_poll
 
 
 def generate_report(application_id):
 
     appl = PlanningApplication.query.filter_by(id=application_id).first()
 
-    recc = get_recommendations(appl.square_footage,appl.pollutants)
+    recc, ol_poll = get_recommendations(appl.square_footage,appl.pollutants)
 
     report = Report(application_id=application_id)
     db.session.add(report)
@@ -96,6 +99,14 @@ def generate_report(application_id):
     for r in recc.keys():
         details = ReportDetail(report_id=report.id,tree_id=r,quantity=recc[r][0],targeted_pollutant=recc[r][1])
         db.session.add(details)
+        db.session.flush()
+
+        t = [te for te in TreeEfficacy.query.filter_by(tree_id = r).all() if te.pollutant_id != recc[r][1] and te.pollutant_id in ol_poll]
+        t = sorted(t, key=lambda x: x.effectiveness, reverse=True)
+        t = t[:3] if len(t) >=3 else t
+        for te in t:
+            tp = TargetedPollutant(tree_id = r, targeted_pollutant_id = te.pollutant_id, report_id = report.id, target_type = TargetedPollutantEnum.SECONDARY.value)
+            db.session.add(tp)
 
     db.session.commit()
 
